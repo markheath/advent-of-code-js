cddfa55f69511bf1fe3824a8733aa13cb02469ea
Mark Heath <mark.heath@gmail.com>
2017-11-22 22:16:49 +0000

    2015 day 8 solution

---
+++ 2015/08/solve.js
@@ -0,0 +1,30 @@
+function solve(input, part) {
+    var solver = part === 1 ? part1 : part2;
+    return solver(input);
+}
+
+function expected(part) {
+    return part == 1 ? 1333 : 2046;
+}
+
+let [q,b,bq,bb] = ["\"","\\","\\\"","\\\\"];
+
+let unescape = function(s) {
+    return s.slice(1,-1).replace(/\\\"/g, q).replace(/\\\\/g, "?").replace(/\\x[0-9a-f]{2}/g,"?");
+}
+
+let escape = s => q + s.replace(/\\/g, bb).replace(/\"/g, bq) + q;
+
+let sumDiffLengths = function(strings,f) {
+    return strings.map(f).reduce((acc,[a,b]) => acc + (a.length - b.length), 0);
+}
+
+function part1(input) {
+    return sumDiffLengths(input.split("\n"), f => [f, unescape(f)]);
+}
+
+function part2(input) {
+    return sumDiffLengths(input.split("\n").slice(0,-1), f => [escape(f), f]);
+}
+
+module.exports = {solve,expected};
