276845d970d94f110bcfc5dd7025b184ddaeb98d
Mark Heath <mark.heath@gmail.com>
2017-11-22 13:49:48 +0000

    2015 day 2 solution

--- 2015/02/solve.js
+++ 2015/02/solve.js
@@ -7,13 +7,25 @@ function expected(part) {
     return part == 1 ? 1598415 : 3812909;
 }
 
+function measure(input, fn) {
+    return input.split("\n")
+    .map(s => s.split('x'))
+    .filter(x => x.length > 1)
+    .map(x => x.map(n => parseInt(n)))
+    .map(w => w.sort((a,b)=>a-b))
+    .map(fn)
+    .reduce((a,b) => a+b);
+}
+
 function part1(input) {
-    return 0;
+    return measure(input, w => 3 * w[0] * w[1] + 2 * w[0] * w[2] + 2 * w[1] * w[2]);
 }
 
 function part2(input) {
-    return 0;
+    return measure(input, w => 2 * w[0] + 2 * w[1] + w[0] * w[1] * w[2]);
 }
+
+
 
 
 module.exports = {solve,expected};
