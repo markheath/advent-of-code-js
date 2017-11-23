d8519636bd1839d7521ba3da3d07950e7b19fcf7
Mark Heath <mark.heath@gmail.com>
2017-11-23 08:32:37 +0000

    2015 day 10 solution

---
+++ 2015/10/solve.js
@@ -0,0 +1,36 @@
+let _ = require('lodash');
+
+function solve(input, part) {
+    var solver = part === 1 ? part1 : part2;
+    return solver(input);
+}
+
+function expected(part) {
+    return part == 1 ? 360154 : 5103798;
+}
+
+let groupAdjacent = a => a.reduce(function(prev, curr) {
+    if (prev.length && curr === prev[prev.length - 1][0]) {
+        prev[prev.length - 1].push(curr);
+    }
+    else {
+        prev.push([curr]);
+    }
+    return prev;
+}, []);
+
+function getRepeatedLength(repetitions,input) {
+    let start = Array.from(input).map(c => c - '0');
+    return _.range(1,repetitions + 1)
+            .reduce((acc,x) => _.flatMap(groupAdjacent(acc),g => [g.length, g[0]]), start).length;
+}
+
+function part1(input) {
+    return getRepeatedLength(40,input);
+}
+
+function part2(input) {
+    return getRepeatedLength(50,input);
+}
+
+module.exports = {solve,expected};
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var _ = require('lodash');
 var chalk = require('chalk');
 var fs = require('fs');
 
 let year = 2015;
-for(let day = 7; day <= 9; day++) {
+for(let day = 7; day <= 10; day++) {
     let path = `./${year}/${("0" + day).slice(-2)}`;
     let solver = require(path +`/solve`);
     var text = fs.readFileSync(path + `/input.txt`).toString();
