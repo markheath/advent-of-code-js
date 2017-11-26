0f5f2476a29378e1a45adc87c21a53503330bdfe
Mark Heath <mark.heath@gmail.com>
2017-11-26 21:35:10 +0000

    solution for 2015 day 17

---
+++ 2015/17/solve.js
@@ -0,0 +1,29 @@
+function solve(input, part) {
+    let containers = input.map(s => Number(s));
+    let perms = Array.from(distribute([], containers, 150, 0));
+    if (part === 1) {
+        return perms.length;
+    }
+    let m = Math.min.apply(null, perms.map(f => f.length));
+    return perms.filter(a => a.length === m).length;
+}
+
+function* distribute(used, pool, target, runningTotal)
+{
+    if (pool.length > 0) {
+        let h = pool[0];
+        let tail = pool.slice(1);
+        if (h + runningTotal === target)
+            yield Array(...used,h);
+        else if (h + runningTotal < target)
+            yield* distribute(Array(...used,h), tail, target, h + runningTotal);
+        yield* distribute(used, tail, target, runningTotal);
+    }
+}
+
+function expected(part) {
+    return part == 1 ? 4372 : 4;
+}
+
+
+module.exports = {solve,expected};
