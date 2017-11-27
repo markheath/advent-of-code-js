683138037953c1ae8ba307f6793deedb9d5e4cd8
Mark Heath <mark.heath@gmail.com>
2017-11-27 08:40:48 +0000

    solution for 2015 day 20

---
+++ 2015/20/solve.js
@@ -0,0 +1,30 @@
+
+
+function solve(input, part) {
+    const target = Number(input[0]);
+
+    if (part === 1) {
+        let houses = new Array(target/10 + 1).fill(0);
+        for (let elf = 1; elf < houses.length; elf++)
+            for (let house = elf; house < houses.length; house+=elf)
+                houses[house] += elf * 10;
+        for (let house = 1; house < houses.length; house++)
+            if (houses[house] > target) { return house; }
+    }
+    else {
+        let houses = new Array(Math.trunc(target/11) + 1).fill(0);
+        for (let elf = 1; elf < houses.length; elf++)
+            for (let house = elf, n = 0; house < houses.length && n < 50; house+=elf, n++)
+                houses[house] += elf * 11;
+        for (let house = 1; house < houses.length; house++)
+            if (houses[house] > target) { return house; }
+    }
+}
+
+
+function expected(part) {
+    return part == 1 ? 831600 : 884520;
+}
+
+
+module.exports = {solve,expected};
