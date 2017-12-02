467d5b34b4fead5cbf161acba257dd42bd16638a
Mark Heath <mark.heath@gmail.com>
2017-12-02 09:27:37 +0000

    tidying up index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var chalk = require('chalk');
-var fs = require('fs');
+const chalk = require('chalk');
+const fs = require('fs');
 
 let year = 2017;
 let startDay = 1;
 let endDay = 25;
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 for(let arg of args) {
-    var n = Number(arg);
+    const n = Number(arg);
     if (n > 2000) {
         year = n;
     } 
@@ -17,20 +17,20 @@ for(let arg of args) {
 }
 
 for(let day = startDay; day <= endDay; day++) {
-    let path = `./${year}/${("0" + day).slice(-2)}`;
+    const path = `./${year}/${("0" + day).slice(-2)}`;
     if (!fs.existsSync(path)) {
         console.log(chalk.red(`${year} day ${day} not found`));
         break;
     }
     let solver = require(path +`/solve`);
-    var text = fs.readFileSync(path + `/input.txt`)
+    const text = fs.readFileSync(path + `/input.txt`)
                     .toString()
                     .split('\n')
                     .map(s => s.replace(/\r$/, ''))
                     .filter(s => s.length > 0);
     for(let part of [1,2]) {
-        let answer = solver.solve(text, part);
-        let expected = solver.expected(part);
+        const answer = solver.solve(text, part);
+        const expected = solver.expected(part);
         if (answer === expected) {
             console.log(chalk.green(`${year} day ${day} part ${part}: ${answer}`));
         }
