918147ad23f3b5b8a6004a712de70ce1d2925cd7
Mark Heath <mark.heath@gmail.com>
2017-12-04 07:40:37 +0000

    2017 day 4 solution part 2

--- 2017/04/solve.js
+++ 2017/04/solve.js
@@ -1,12 +1,27 @@
 function solve(input, part) {
-    console.log(isValid("aa bb cc dd ee"));
-    console.log(isValid("aa bb cc dd aa"));
-    console.log(isValid("aa bb cc dd aaa"));
-    return input.filter(isValid).length;
+    if (part === 1) {
+        console.log(isValid("aa bb cc dd ee")); // true
+        console.log(isValid("aa bb cc dd aa")); // false
+        console.log(isValid("aa bb cc dd aaa")); // true
+        return input.filter(isValid).length;
+    }
+    else {
+        console.log(isValid2("abcde fghij")); // true
+        console.log(isValid2("abcde xyz ecdab")); // false
+        console.log(isValid2("a ab abc abd abf abj")); // true
+        console.log(isValid2("iiii oiii ooii oooi oooo")); // true
+        console.log(isValid2("oiii ioii iioi iiio")); // false
+        return input.filter(isValid2).length;
+    }
 }
 
 function isValid(passphrase) {
     const words = passphrase.split(' ');
+    return words.length === new Set(words).size;
+}
+
+function isValid2(passphrase) {
+    const words = passphrase.split(' ').map(w => [...w].sort().join(''));
     return words.length === new Set(words).size;
 }
 
