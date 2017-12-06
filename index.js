932bff365a5773dc3ad99400a7cb7f5603a5293d
Mark Heath <mark.heath@gmail.com>
2017-12-06 12:47:22 +0000

    adding timings to puzzle runner output

--- index.js
+++ index.js
@@ -16,26 +16,39 @@ for(let arg of args) {
     }
 }
 
+const timed = fn => {
+    const start = process.hrtime();
+    const output = fn();
+    const [secs,nanosecs] = process.hrtime(start);
+    const duration = secs*1000 + Math.floor(nanosecs/1000000);
+    return [output, duration];
+}
+
+const showTestResult = (year, day, part, expected, actual, duration) => {
+    const durationDesc = chalk.blue(` (${duration}ms)`);
+    if (actual === expected) {
+        console.log(chalk.green(`${year} day ${day} part ${part}: ${actual}`) + durationDesc);
+    }
+    else {
+        console.log(chalk.red(`${year} day ${day} part ${part}: ${actual} - expected ${expected}` + durationDesc));
+    }
+}
+
 for(let day = startDay; day <= endDay; day++) {
     const path = `./${year}/${("0" + day).slice(-2)}`;
     if (!fs.existsSync(path)) {
         console.log(chalk.red(`${year} day ${day} not found`));
         break;
     }
-    let solver = require(path +`/solve`);
+    const solver = require(path +`/solve`);
     const text = fs.readFileSync(path + `/input.txt`)
                     .toString()
                     .split('\n')
                     .map(s => s.replace(/\r$/, ''))
                     .filter(s => s.length > 0);
     for(let part of [1,2]) {
-        const answer = solver.solve(text, part);
         const expected = solver.expected(part);
-        if (answer === expected) {
-            console.log(chalk.green(`${year} day ${day} part ${part}: ${answer}`));
-        }
-        else {
-            console.log(chalk.red(`${year} day ${day} part ${part}: ${answer} - expected ${expected}`));
-        }
+        const [answer,duration] = timed(() => solver.solve(text, part));
+        showTestResult(year, day, part, expected, answer, duration);
     }
 }
