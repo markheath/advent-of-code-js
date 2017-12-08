f13e8f91ffcf3670cc2760a70c6ea3bcaf1c6e02
Mark Heath <mark.heath@gmail.com>
2017-12-08 19:02:21 +0000

    simplify day 8 solution

--- 2017/08/solve.js
+++ 2017/08/solve.js
@@ -1,47 +1,33 @@
-let { maxBy } = require('../../utils/utils')
+let { max } = require('../../utils/utils')
 
-let largest = 0;
 function solve(input, part) {
     const instructions = input.map(parseInstruction);
-    largest = 0
     let state = new Map()
-    executeInstructions(state,instructions)
-    if (part === 1) {
-        let maxReg = maxBy(state, ([,val]) => val)
-        //console.log(maxReg)
-        return maxReg[1];
-    }
-    else {
-        return largest;
-    }
+    const largest = executeInstructions(state,instructions)
+    return (part === 1) ? max(state.values()) : largest;
 }
 
-const testInstructions = [
-    "b inc 5 if a > 1",
-    "a inc 1 if b < 5",
-    "c dec -10 if a >= 1",
-    "c inc -20 if c == 10",
-]
-
-// g dec 445 if fwk == -3145
-// pnt inc 428 if xek < 10
 function parseInstruction(instruction) {
     let g = /(\w+) (\w+) (-?\d+) if (\w+) ([<>=!]+) (-?\d+)/.exec(instruction)
-    //if (!g[2]) console.log(instruction)
     return { target: g[1], op: g[2], amount: Number(g[3]), testReg: g[4], testOp: g[5], testAmount: Number(g[6]) };
 }
 
 function executeInstructions(registers, instructions) {
+    let largest = 0;
     for (let i of instructions) {
-        if (testRegister(registers.get(i.testReg) || 0, i.testOp, i.testAmount)) {
+        if (applyOp(registers.get(i.testReg) || 0, i.testOp, i.testAmount)) {
             let newValue = applyOp(registers.get(i.target) || 0, i.op, i.amount)
             largest = Math.max(largest,newValue)
             registers.set(i.target, newValue);
         }
     }
+    return largest;
 }
 
-function testRegister(value, op, amount) {
+const ops = {'<':(a,b)=>a<b,'>':(a,b)=>a>b,'<=':(a,b)=>a<=b,'>=':(a,b)=>a>=b,'!=':(a,b)=>a!==b,'==':(a,b)=>a===b,'inc':(a,b)=>a+b,'dec':(a,b)=>a-b };
+const applyOp = (val,op,amt) => ops[op](val,amt)
+
+function testRegister1(value, op, amount) {
     switch(op) {
         case '<': return value < amount;
         case '>': return value > amount;
@@ -53,7 +39,7 @@ function testRegister(value, op, amount) {
     }
 }
 
-function applyOp(value, op, amount) {
+function applyOp1(value, op, amount) {
     switch(op) {
         case 'inc': return value + amount;
         case 'dec': return value - amount;
