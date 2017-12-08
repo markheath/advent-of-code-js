7a6daccc4adbc6c54ee35d532179bb92bd5e5b79
Mark Heath <mark.heath@gmail.com>
2017-12-08 07:59:33 +0000

    2017 day 8 part 1 solution

---
+++ 2017/08/solve.js
@@ -0,0 +1,58 @@
+let { maxBy } = require('../../utils/utils')
+
+function solve(input, part) {
+    const instructions = input.map(parseInstruction);
+    let state = new Map()
+    executeInstructions(state,instructions)
+    let maxReg = maxBy(state, ([key,val]) => val)
+    console.log(state)
+    console.log(maxReg)
+    return part; // 1915 wrong
+}
+
+const testInstructions = [
+    "b inc 5 if a > 1",
+    "a inc 1 if b < 5",
+    "c dec -10 if a >= 1",
+    "c inc -20 if c == 10",
+]
+
+// g dec 445 if fwk == -3145
+// pnt inc 428 if xek < 10
+function parseInstruction(instruction) {
+    let g = /(\w+) (\w+) (-?\d+) if (\w+) ([<>=!]+) (-?\d+)/.exec(instruction)
+    //if (!g[2]) console.log(instruction)
+    return { target: g[1], op: g[2], amount: Number(g[3]), testReg: g[4], testOp: g[5], testAmount: Number(g[6]) };
+}
+
+function executeInstructions(registers, instructions) {
+    for (let i of instructions) {
+        if (testRegister(registers.get(i.testReg) || 0, i.testOp, i.testAmount)) {
+            registers.set(i.target, applyOp(registers.get(i.target) || 0, i.op, i.amount));
+        }
+    }
+}
+
+function testRegister(value, op, amount) {
+    switch(op) {
+        case '<': return value < amount;
+        case '>': return value > amount;
+        case '<=': return value <= amount;
+        case '>=': return value >= amount;
+        case '!=': return value !== amount;
+        case '==': return value === amount;
+        default: throw new Error("invalid test op " + op)
+    }
+}
+
+function applyOp(value, op, amount) {
+    switch(op) {
+        case 'inc': return value + amount;
+        case 'dec': return value - amount;
+        default: throw new Error("invalid op " + op)
+    }
+}
+
+const expected = part => part === 1 ? -1 : -1;
+
+module.exports = {solve,expected};
