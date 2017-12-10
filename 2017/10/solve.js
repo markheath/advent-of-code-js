932d9394fe761557eaa580f22f2bd32669d89c4e
Mark Heath <mark.heath@gmail.com>
2017-12-10 13:10:46 +0000

    2017 day 10 solution part 2

--- 2017/10/solve.js
+++ 2017/10/solve.js
@@ -1,23 +1,38 @@
 function solve(input, part) {
-    let start = new Array(256);
-    for(let n = 0; n < start.length; n++) start[n] = n;
 
     if (part === 1) {
-        let lengths = input[0].split(',').map(n => Number(n))
+        let start = new Array(256);
+        for(let n = 0; n < start.length; n++) start[n] = n;
+            let lengths = input[0].split(',').map(n => Number(n))
         let {result} = applyLengths(start,lengths)
         return result[0] * result[1];
     }
     else {
-        let lengths = [...input[0]].map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23])
-        let currentPos = 0
-        let skipSize = 0
-        for(let n = 0; n < 64; n++) {
-            ({result:start,currentPos,skipSize} = applyLengths(start,lengths,currentPos,skipSize));
-        }
+        return hashString(input[0])
     }
 }
 
-const expected = part => part === 1 ? 20056 : -1;
+const expected = part => part === 1 ? 20056 : "d9a7de4a809c56bf3a9465cb84392c8e";
+
+function hashString(str) {
+    let start = new Array(256);
+    for(let n = 0; n < start.length; n++) start[n] = n;
+    let lengths = [...str].map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23])
+    let currentPos = 0
+    let skipSize = 0
+    for(let n = 0; n < 64; n++) {
+        ({result:start,currentPos,skipSize} = applyLengths(start,lengths,currentPos,skipSize));
+    }
+    let hash = ""
+    for(let n = 0; n < 16; n++) {
+        let xor = 0
+        for(let x = 0; x<16; x++) {
+            xor ^= start[n*16+x]
+        }
+        hash+= ("0" + xor.toString(16)).slice(-2)
+    }
+    return hash;
+}
 
 function applyLengths(list, lengths, currentPos=0,skipSize=0) {
     for(let length of lengths) {
@@ -35,4 +50,4 @@ function reverseSection(list, currentPos, length) {
     return out;
 }
 
-module.exports = {solve,expected,reverseSection,applyLengths};
+module.exports = {solve,expected,reverseSection,applyLengths,hashString};
--- spec/2017-10-Spec.js
+++ spec/2017-10-Spec.js
@@ -1,4 +1,4 @@
-const { reverseSection,applyLengths } = require("../2017/10/solve")
+const { reverseSection,applyLengths,hashString } = require("../2017/10/solve")
 
 const reverseTests = [ [[0,1,2,3,4], 0, 2, [1,0,2,3,4] ],
 [[0,1,2,3,4], 1, 3, [0,3,2,1,4] ],
@@ -6,6 +6,12 @@ const reverseTests = [ [[0,1,2,3,4], 0, 2, [1,0,2,3,4] ],
 [[0,1,2,3,4], 3, 3, [3,1,2,0,4] ],
 ]
 
+const hashTests = [
+    ["", "a2582a3a0e66e6e86e3812dcb672a272"],
+    ["AoC 2017" , "33efeb34ea91902bb2f59c9920caa6cd"],
+    ["1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"],
+    ["1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"]
+]
 
 describe("2017 day 10", function() {
     it ("can reverse sections", function() {
@@ -16,5 +22,10 @@ describe("2017 day 10", function() {
     it ("can apply lengths", function() {
         let {result} = applyLengths([0,1,2,3,4],[3, 4, 1, 5]);
         expect(result).toEqual([3,4,2,1,0]);
+    })
+
+    it ("can hash strings", function() {
+        for (let [input,expected] of hashTests)
+            expect(hashString(input)).toEqual(expected);
     })
 });
