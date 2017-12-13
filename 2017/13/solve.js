b76fd32f3375d1c2b6fa519b69ea13f93fd9077e
Mark Heath <mark.heath@gmail.com>
2017-12-13 08:12:08 +0000

    2017 day 13 part 2

--- 2017/13/solve.js
+++ 2017/13/solve.js
@@ -1,21 +1,44 @@
 const {max} = require('../../utils/utils')
 function solve(input, part) {
-    const firewall = new Map( input.map(s => /(\d+): (\d+)/.exec(s))
+    const firewall = loadFirewall(input)
+    if (part === 1) {
+        return scoreTrip(firewall,0).score
+    }
+    else {
+        let f = loadFirewall(input)
+        for(let delay = 0; ; delay++) {
+            let {wasCaught} = scoreTrip(f,delay)
+            //console.log(`delay ${delay}: score ${score}`)
+            if (!wasCaught) return delay;
+        }
+    }
+}
+
+function loadFirewall(input) {
+    return new Map( input.map(s => /(\d+): (\d+)/.exec(s))
     .map(([,d,r]) => ([Number(d),Number(r)])))
+}
+
+function scoreTrip(firewall, delay) {
     const maxDepth = max(firewall.keys())
     
     let score = 0;
+    let wasCaught = false;
     for(let depth = 0; depth<=maxDepth; depth++) {
         let range = firewall.get(depth) || 0
         if (range > 0) {
             let period = 2 * (range - 1)
-            let caught = depth%period === 0;
-            if (caught)
-                score += depth*range;
+            let currentTime = delay + depth
+            let caught = currentTime%period === 0;
+            if (caught) {
+                score += (depth*range);
+                wasCaught = true;
+            }
         }
     }
-    return score;
+    return {score,wasCaught};
 }
+
 
 const testInput = [
 "0: 3",
@@ -23,7 +46,6 @@ const testInput = [
 "4: 4",
 "6: 4" ]
 
-
-const expected = part => part === 1 ? 1612 : -1;
+const expected = part => part === 1 ? 1612 : 3907994;
 
 module.exports = {solve,expected};
