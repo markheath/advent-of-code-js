fcdf1f98c5d96914842d3da1b8e7f8e7d0b302eb
Mark Heath <mark.heath@gmail.com>
2017-12-17 09:08:42 +0000

    speed up part 2

--- 2017/17/solve.js
+++ 2017/17/solve.js
@@ -1,7 +1,5 @@
 
 function solve(input, part) {
-    //let buf = {val:0}
-    //buf.next = buf;
     let steps = Number(input[0]) // for test 3
     if (part === 1) {
         let buf = spinFor(2017,steps)
@@ -11,39 +9,18 @@ function solve(input, part) {
         let second = -1
         let curPos = 0;
         for(let n = 1; n < 50000000; n++) {
-            let newSecond
-            [curPos,newSecond] = spin2(steps,curPos,n,second)
-            if (newSecond != second) {
-                //console.log(n,second)
-                second = newSecond
-            }
+            curPos = (curPos + steps)%n + 1
+            if (curPos === 1) second = n;
         }
         return second;
-        //let buf = spinFor(50000000,steps)
-        //return buf[buf.indexOf(0)+1]
     }
 }
 
-//with steps = 3:
-// 1: 1, 
-// 2: 2x3, 
-// 5x4, 
-// 9x3, 
-// 12x4, 
-// 16xlots?, 
-// 218: 218x
-
-//with steps = 348
-// 1 13 22 34 57
-
-function spinFor(iterations,steps,debug) {
+function spinFor(iterations,steps) {
     let buf = [0]
     let curPos = 0;
     for(let n = 1; n <= iterations; n++) {
         curPos = spin(buf,steps,curPos,n)
-        if (debug) console.log(n,buf.slice(0,2))
-        //console.log(buf,curPos)
-        
     }
     return buf;
 }
@@ -52,11 +29,6 @@ function spin(buffer, steps, curPos, val) {
     const insertPos = (curPos + steps)%buffer.length + 1
     buffer.splice(insertPos, 0, val);
     return insertPos;
-}
-
-function spin2(steps, curPos, val, second) {
-    const insertPos = (curPos + steps)%val + 1
-    return [insertPos, (insertPos === 1) ? val : second]
 }
 
 const expected = part => part === 1 ? 417 : 34334221;
