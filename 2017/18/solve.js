e273b6c35491a2fe9d2768137d4a5f83f6d5b77c
Mark Heath <mark.heath@gmail.com>
2017-12-18 11:01:02 +0000

    code cleanup day 18

--- 2017/18/solve.js
+++ 2017/18/solve.js
@@ -2,7 +2,7 @@ function solve(input,part) {
     const instructions = input.map(i => i.split(' '))
     if (part === 1) {
         let sent = -1
-        runToReceive(interpreter(instructions, 0, n => sent = n, () => undefined))
+        runUntilBlocked(interpreter(instructions, 0, n => sent = n, () => undefined))
         return sent;
     } 
     else {
@@ -12,38 +12,12 @@ function solve(input,part) {
         let totalSent1 = 0;
         let i1 = interpreter(instructions, 1, n => { sent1.push(n); totalSent1++ }, () => sent0.shift())
         do {
-            //console.log("running p0, available:", sent1.length)
-            runToReceive(i0)
-            //console.log("running p1, available:", sent0.length)
-            runToReceive(i1)
-            //console.log(sent0.length, sent1.length, i0.registers, i1.registers)
+            runUntilBlocked(i0)
+            runUntilBlocked(i1)
         } while(sent0.length > 0 || sent1.length > 0)
         return totalSent1
     }
 }
-
-const testInput = [
-    "set a 1",
-    "add a 2",
-    "mul a a",
-    "mod a 5",
-    "snd a",
-    "set a 0",
-    "rcv a",
-    "jgz a -1",
-    "set a 1",
-    "jgz a -2",
-]
-
-const testInput2 = [
-    "snd 1",
-    "snd 2",
-    "snd p",
-    "rcv a",
-    "rcv b",
-    "rcv c",
-    "rcv d"
-]
 
 function interpreter(instructions, id, send, recv) {
     const registers = new Map();
@@ -52,7 +26,6 @@ function interpreter(instructions, id, send, recv) {
     let curPos = 0
     const getVal = v => isNaN(v) ? (registers.get(v) || 0) : Number(v)
     const commands = {
-        //snd : reg => { console.log(`prog ${id} sends ${getVal(reg)}`); send(getVal(reg)) },
         snd : x => send(getVal(x)),
         set : (x,y) => registers.set(x,getVal(y)),
         add : (x,y) => registers.set(x,getVal(x) + getVal(y)),
@@ -64,7 +37,6 @@ function interpreter(instructions, id, send, recv) {
     const execute = () => {
         let [ins,arg1,arg2] = instructions[curPos]
         commands[ins](arg1,arg2)
-        //console.log(curPos,ins,arg1,arg2,commands[ins])
         if (!blocked && ins != "jgz") curPos++
         if(curPos < 0 || curPos >= instructions.length) throw Error(`program ${id} exited`)
         return blocked
@@ -75,12 +47,11 @@ function interpreter(instructions, id, send, recv) {
     return {execute,showState}
 }
 
-function runToReceive(interpreter) {
-    //interpreter.showState()
+function runUntilBlocked(interpreter) {
     while (!interpreter.execute()) { //
     }
 }
 
-const expected = part => part === 1 ? 8600 : "todo"
+const expected = part => part === 1 ? 8600 : 7239
 
 module.exports = { solve, expected}
