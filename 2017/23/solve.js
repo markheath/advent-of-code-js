4b65a5fe700449f7938acfc653fd69e35c867ca4
Mark Heath <mark.heath@gmail.com>
2017-12-23 09:07:32 +0000

    next step optimize part 2

--- 2017/23/solve.js
+++ 2017/23/solve.js
@@ -43,12 +43,11 @@ function interpreter(instructions, a) {
 }
 
 
-function part2() {
-    let a,b,c,d,e,f,g,h
+function part2(a) {
+    let b,c,d,e,f,h
     b = 67                      // set b 67
     c = b                       // set c b
-    if (a !== 0) {              // jnz a 2
-                                // jnz 1 5
+    if (a !== 0) {              // jnz a 2, jnz 1 5
         b *= 100                // mul b 100
         b += 100000             // sub b -100000
         c = b                   // set c b
@@ -56,24 +55,17 @@ function part2() {
     do { f = 1                  // set f 1
         d = 2                   // set d 2
         do {e = 2               // set e 2
-            do { g = d          // set g d
-                g *= e          // mul g e
-                g -= b          // sub g b
-                if (g === 0)    // jnz g 2
+            do { 
+                if ((d * e) === b)    // set g d, mul g e, sub g b, jnz g 2
                     f = 0       // set f 0
                 e++             // sub e -1
-                g = e           // set g e
-                g -= b          // sub g b
-            } while (g != 0)    // jnz g -8
+            } while (e != b)    // set g e, sub g b, jnz g -8
             d++                 // sub d -1
-            g = d               // set g d
-            g -= b              // sub g b
-        } while (g != 0)        // jnz g -13
+        } while (d != b)        // set g d, sub g b, jnz g -13
         if(f ===0)              // jnz f 2
             h++                 // sub h -1
-        g = b                   // set g b // these last 4 lines mean loop x1 in part 1 and x1000 in part 2
-        g -= c                  // sub g c
-        if (g === 0)            // jnz g 2
+        // these last 4 lines mean loop x1 in part 1 and x1000 in part 2
+        if (b === c)            // set g b,sub g c, jnz g 2
             return h            // jnz 1 3
         b += 17                 // sub b -17
     } while(true)               // jnz 1 -23
