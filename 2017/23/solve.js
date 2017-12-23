c83c0bb7aafb97638562481bb5324a0989b5038e
Mark Heath <mark.heath@gmail.com>
2017-12-23 08:44:22 +0000

    trying to optimize part 2 step 1

--- 2017/23/solve.js
+++ 2017/23/solve.js
@@ -1,14 +1,26 @@
 function solve(input, part) {
     const instructions = input.map(i => i.split(' '))
-    const int = interpreter(instructions)
+
+    if (part === 1) {
+        const int = interpreter(instructions, 0)
+        while(!int.execute()) {
+            //
+        }
+        return int.mult();    
+    }
+
+    const int = interpreter(instructions, 1)
+    let it = 0
     while(!int.execute()) {
-        //
+        int.showState()
+        if (it++ > 10) break;
     }
     return int.mult();
 }
 
-function interpreter(instructions) {
+function interpreter(instructions, a) {
     const registers = new Map();
+    registers.set('a',a)
     let curPos = 0
     let multiplies = 0;
     const getVal = v => isNaN(v) ? (registers.get(v) || 0) : Number(v)
@@ -29,6 +41,44 @@ function interpreter(instructions) {
     }
     return {execute,showState,mult:() => multiplies}
 }
+
+
+function part2() {
+    let a,b,c,d,e,f,g,h
+    b = 67
+    c = b
+    if (a !== 0) {
+        //jnz 1 5
+        b *= 100
+        b += 100000
+        c = b
+        c += 17000 }
+    do { f = 1
+        d = 2
+        do {e = 2
+            do { g = d
+                g *= e
+                g -= b
+                if (g === 0)
+                    f = 0
+                e++
+                g = e
+                g -= b
+            } while (g != 0)
+            d++
+            g = d
+            g -= b
+        } while (g != 0)
+        if(f ===0)
+            h++
+        g = b // these last 4 lines mean loop x1 in part 1 and x1000 in part 2
+        g -= c
+        if (g === 0)
+            return h
+        b += 17
+    } while(true)
+}
+
 
 const expected = part => part === 1 ? 4225 : "todo"
 
