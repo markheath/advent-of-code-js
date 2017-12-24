ee03be2fb67d43b3e07a7a077e9f34c1e06e9a31
Mark Heath <mark.heath@gmail.com>
2017-12-24 09:27:49 +0000

    cleanup day 24

--- 2017/24/solve.js
+++ 2017/24/solve.js
@@ -1,24 +1,21 @@
 const { maxBy} = require('../../utils/utils')
 
+let bridges;
 function solve(input,part) {
     let parts = input.map(i => i.split('/').map(n => Number(n)))
+    bridges = bridges || [...build(0,[],parts,0)]
     if (part === 1) {
-        return maxBy(build(0,[],parts,0),c => c.strength).strength
+        return maxBy(bridges,c => c.strength).strength
     }
     else {
-        let bridges = [...build(0,[],parts,0)]
-        let longest = maxBy(bridges,c => c.used.length).used.length
-        return maxBy(bridges.filter(b => b.used.length === longest),b => b.strength).strength
+        return bridges.sort((a,b) => b.used.length - a.used.length || b.strength - a.strength)[0].strength
     }
 }
 
-function *build(current,used,available,strength) {
+function *build(cur,used,available,strength) {
     for(let [a,b] of available) {
-        if(a === current) {
-            yield* build(b,[...used,[a,b]],available.filter(([x,y]) => !(x===a && y===b)),strength+a+b)
-        }
-        else if (b === current && a !== b) {
-            yield* build(a,[...used,[a,b]],available.filter(([x,y]) => !(x===a && y===b)),strength+a+b)
+        if(a === cur || b === cur) {
+            yield* build(a === cur ? b : a,[...used,[a,b]],available.filter(([x,y]) => !(x===a && y===b)),strength+a+b)
         }
     }
     yield {used,strength}
