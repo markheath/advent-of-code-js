a2a27b1e9a2141ae7beba675a91feab83d945d17
Mark Heath <mark.heath@gmail.com>
2017-12-25 15:44:22 +0000

    day 25 solution uses input

--- 2017/25/solve.js
+++ 2017/25/solve.js
@@ -1,17 +1,17 @@
+const { batch,skip } = require('../../utils/utils')
+
 function solve(input,part) {
     if (part === 2) return 0
-    let state = 'A'
-    const steps = 12481997
-    let cursor = 0
-    let tape = {}
-
-    let rules =
-        {'A': [1,1,'B',0,-1,'C'],
-         'B': [1,-1,'A',1,1,'D'],
-         'C': [0,-1,'B',0,-1,'E'],
-         'D': [1,1,'A',0,1,'B'],
-         'E': [1,-1,'F',1,-1,'C'],
-         'F': [1,1,'D',1,1,'A']}
+    let state = input[0].match(/Begin in state (.)\./)[1]
+    const steps = Number(input[1].match(/after (\d+) steps/)[1])
+    let cursor = 0, tape = {}, rules = {}
+    const parseState = s => s.match(/state (.)/)[1] 
+    const parseVal = s => Number(s.match(/the value (\d)\./)[1])
+    const parseDir = s => s.match(/to the right/) ? 1 : -1 
+    for(let b of batch(skip(input,2),9)) { // nb blank input lines already stripped
+        rules[parseState(b[0])] = [parseVal(b[2]),parseDir(b[3]),parseState(b[4]),
+                                    parseVal(b[6]),parseDir(b[7]),parseState(b[8])]
+    }
 
     for(let step = 0; step < steps; step++) {
         let rule = rules[state];
